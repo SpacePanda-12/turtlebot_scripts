247bbceb1e663788d09a2275a4c866d7e766c924
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2021-06-25 09:45:38 -0400

    Create message_creator.py
    script that chooses which message to output to console

---
+++ message_creator.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env
+
+import rospy
+
+# simple if else for low battery warning
+def create_message(twist, batteryLevel, posX, posY, orient_x, orient_y, orient_z, orient_w):
+    if(batteryLevel>=20):
+        msg = """
+        Sending commands to turtlebot...
+        --------------------------------
+        Linear velocities
+        X: %f
+        Y: %f
+        Angular Velocity: %f
+        --------------------------------
+        Odometry
+        X Position: %f
+        Y Position: %f
+        --------------------------------
+        Orientation (IMU)
+        x: %f
+        y: %f
+        z: %f
+        w: %f
+        --------------------------------
+        Estimated battery level: %f/100
+        --------------------------------
+        
+        --------------------------------
+
+        """
+    else:
+        msg = """
+        Sending commands to turtlebot...
+        --------------------------------
+        Linear velocities
+        X: %f
+        Y: %f
+        Angular Velocity: %f
+        --------------------------------
+        Odometry
+        X Position: %f
+        Y Position: %f
+        --------------------------------
+        Estimated battery level: %f/100
+        BATTERY LEVEL LOW. CHARGE SOON
+        --------------------------------
+        
+
+
+
+
+
+        --------------------------------
+
+        """
+
+    # write the above message to the console with velocity, position, battery level values
+    rospy.loginfo(msg, twist.linear.x, twist.linear.y, twist.angular.z, posX, posY,
+    orient_x, orient_y, orient_z, orient_w, batteryLevel)
+    
