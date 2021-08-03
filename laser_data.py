1f8a0a0d43506bf7cfe21849fae17bd3f405a264
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2021-08-03 15:40:27 -0400

    Implemented PID Control
    Implemented a PID class. Running script as is will make the turtlebot move in a square. However, issues with turtlebot odometry (and possibly the wheels having uneven torques?) are causing errors in rotation and movement; i.e. the script will move the turtlebot in a square but it will not end exactly where it started off. This is because of small rotational errors that accumulate as the turtlebot continually moves and rotates. Will not be able to fix this until we have access to a more accurate positioning system

--- laser_data.py
+++ laser_data.py
@@ -2,23 +2,11 @@
 distance_to_object = 0.0
 distance_to_object_right = 0.0
 distance_to_object_left = 0.0
-xvelocity = 0
-zangular = 0
 
 def get_laser_data(msg):
-    global xvelocity
-    global zangular
     global distance_to_object
     global distance_to_object_left
     global distance_to_object_right
     distance_to_object = msg.ranges[0]
     distance_to_object_left = msg.ranges[90]
     distance_to_object_right = msg.ranges[270]
-
-    if distance_to_object > 0.2 and distance_to_object_left > 0.2 and distance_to_object_right > 0.2:
-        xvelocity = 0.1
-        zangular = 0
-    else:
-        xvelocity = 0
-        zangular = 3.141592654
-    
--- run_turtlebot_node.py
+++ run_turtlebot_node.py
@@ -5,29 +5,20 @@ Script combining functionality of all publisher and subscriber
 nodes for the turtlebot into one interface
 """
 
-import pid_control
-from numpy.lib.function_base import diff
+# import python and ros libraries
 import rospy
-import actionlib
-
 from geometry_msgs.msg import Twist, Point
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import BatteryState, Imu, LaserScan
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-import numpy as np
-from math import radians, copysign, sqrt, pow, pi, atan2
+from sensor_msgs.msg import BatteryState, LaserScan
+from math import sqrt, pi
 from actionlib_msgs.msg import *
 from sound_play.libsoundplay import SoundClient
 import tf
 from tf.transformations import euler_from_quaternion
-from tf.transformations import quaternion_from_euler
-import subprocess
 import os
 import time
-import math
 
-
-
+# import other realm-specific scripts
+import pid_control
 import battery_status
 import odometry_status
 import message_creator
@@ -51,6 +42,8 @@ class turtlebot(object):
         # create a node name run_turtlebot_node
         rospy.init_node('run_turtlebot_node', anonymous=True)
 
+        # set update rate; i.e. how often we send commands (Hz)
+        self.rate = rospy.Rate(10)
 
         rospy.Subscriber('/scan', LaserScan, laser_data.get_laser_data)
 
@@ -72,9 +65,6 @@ class turtlebot(object):
         # allows us to send velocity and rotation commands
         self.move_command = Twist()
 
-        # set update rate; i.e. how often we send commands (Hz)
-        self.rate = rospy.Rate(10)
-
         # create transform listener to transform coords from turtlebot frame to absolute frame
         self.listener = tf.TransformListener()
 
@@ -92,15 +82,25 @@ class turtlebot(object):
                 rospy.signal_shutdown("tf Exception")
 
         #TODO these are hardcoded for testing, but later set this up as an input
-        self.p_gain = .1
-        self.d_gain = 0.05
-        self.i_gain = 0.03
+        self.p_gain_linear = 0.05
+        self.d_gain_linear = 0.02
+        self.i_gain_linear = 0.01
+
+        self.p_gain_angular = .01
+        self.d_gain_angular = 0.005
+        self.i_gain_angular = 0.003
+
         self.i_min = 0.0001
-        self.i_max = 0.1
+        self.i_max = 2
 
-        self.controller = pid_control.pid_controller(self.p_gain, self.d_gain, self.i_gain, self.i_min, self.i_max)
+        self.linear_controller = pid_control.pid_controller(
+            self.p_gain_linear, self.d_gain_linear, self.i_gain_linear, self.i_min, self.i_max
+            )
+        
+        self.angular_controller = pid_control.pid_controller(
+            self.p_gain_angular, self.d_gain_angular, self.i_gain_angular, self.i_min, self.i_max
+        )
 
-        (self.position, self.rotation) = self.get_odom()
         self.first_turn = True
 
 
@@ -132,55 +132,176 @@ class turtlebot(object):
         odometry_status.orient_y, odometry_status.orient_z, odometry_status.orient_w)
     
 
-    def move_forward(self, goal_x, goal_y):
-        (self.position, self.rotation) = self.get_odom()
-        current_distance = sqrt(self.position.x**2 + self.position.y**2)
-        goal_distance = sqrt(goal_x**2+goal_y**2)
+    def move_turtlebot(self, goal_distance):
+        """
+
+        Moves the turtlebot in a straight line to the goal position
+
+        """
+
+        # reset odometry and timeout after 3 seconds 
+        os.system("timeout 1.5 rostopic pub /reset std_msgs/Empty '{}'")
         
-        while goal_distance - current_distance > 0.005:
-            command = self.controller.get_command(current_distance, goal_distance)
+        max_command = 0.2
+
+        (self.position, _) = self.get_odom()
+
+        if goal_distance > 0:
+            current_distance = sqrt((self.position.x)**2 + (self.position.y)**2)
+        else:
+            current_distance = -sqrt((self.position.x)**2 + (self.position.y)**2)
+
+
+        while abs(goal_distance - current_distance) > .01*abs(goal_distance):
+            
+            command = self.linear_controller.get_command(current_distance, goal_distance)
+            
+            if command > max_command:
+                command = max_command
+            elif command < -max_command:
+                command = max_command
+
             self.move_command.linear.x = command
             self.move_command.angular.z = 0
+
             self.command_publisher.publish(self.move_command)
-            (self.position, self.rotation) = self.get_odom()
-            current_distance = sqrt(self.position.x**2 + self.position.y**2)
-            print(self.position.x)
-    
-    def rotate_turtlebot(self, goal_x, goal_y):
+            (self.position, _) = self.get_odom()
+
+            if goal_distance > 0:
+                current_distance = sqrt((self.position.x)**2 + (self.position.y)**2)
+            else:
+                current_distance = -sqrt((self.position.x)**2 + (self.position.y)**2)
+
+            rospy.sleep(0.01)
+
+
+    def rotate_turtlebot(self, goal_rotation):
+        """
+
+        Rotates the turtlebot towards the goal position
         
+        """
+        # reset odometry and timeout after 3 seconds 
+        os.system("timeout 1.5 rostopic pub /reset std_msgs/Empty '{}'")
 
-        (self.position, self.rotation) = self.get_odom()
-        path_angle = atan2(goal_y - self.position.y, goal_x - self.position.x)
-        goal_rotation = path_angle
+        # if the angle is positive, make sure it's
+        # between 0 and 360
+        if goal_rotation > 360:
+            while goal_rotation > 360:
+                goal_rotation -= 360
+        
+        # if the angle is between 180 and 360, then change
+        # the angle to be equivalent but negative to reduce
+        # turning time, i.e. change a 270 degree spin to a
+        # 90 degree spin
+        if 180 < goal_rotation <= 360:
+            goal_rotation -= 360
+        
+        # if the angle is negative, make sure it's between
+        # -360 and 0
+        if goal_rotation < -360:
+            while goal_rotation < -360:
+                goal_rotation += 360
+        
+        # if the angle is between -180 and -360, then change the angle 
+        # to be equivalent but positive to reduce turning time
+        # i.e. change a -270 degree spin to a 90 degree spin
+        if -360 <= goal_rotation < -180:
+            goal_rotation += 360
 
-        while abs(goal_rotation - self.rotation) > 0.01:
+        # convert to radians
+        goal_rotation = goal_rotation/360*2*pi
+
+        # get current rotation
+        (_, self.rotation) = self.get_odom()
+
+
+        max_command = 1
+        start_time = time.time()
+
+        while abs(goal_rotation - (self.rotation)) > 0.25*2*pi/360:
+
+            # break the loop if it takes more than 10 seconds
+            current_time = time.time()
+            if (current_time - start_time) > 15:
+                break
+
+            (_, self.rotation) = self.get_odom()
+
+            # keep the rotation positive and within [0, 2*pi]
+            if goal_rotation > 0:
+                if self.rotation < -2*pi:
+                    self.rotation += 4*pi
+                elif self.rotation < 0:
+                    self.rotation = -self.rotation
+                elif self.rotation >= 2*pi:
+                    self.rotation -= 2*pi
+
+            # keep the rotation negative and within [-2*pi, 0]
+            if goal_rotation < 0:
+                if self.rotation  > 2*pi:
+                    self.rotation -= 4*pi
+                elif self.rotation  > 0:
+                    self.rotation = -self.rotation
+                elif self.rotation  <= -2*pi:
+                    self.rotation += 2*pi
             
-            command = self.controller.get_command(self.rotation, goal_rotation)
+            # evaluate PID command based on goal and state
+            command = self.angular_controller.get_command(self.rotation, goal_rotation)
+
+            # set ceiling and floor for command so turtlebot doesn't turn too quickly
+            if command > max_command:
+                command = max_command
+            elif command < -max_command:
+                command  = -max_command
+
+            # set and publish the linear and angular commands
             self.move_command.linear.x = 0
             self.move_command.angular.z = command
             self.command_publisher.publish(self.move_command)
-            (self.position, self.rotation) = self.get_odom()
-            print(command)
-            rospy.sleep(0.1)
-            # print("HELLO WORLD")
-            # print(self.controller.command)
-    
 
-
-
-
+            # sleep to avoid overloading turtlebot with commands
+            rospy.sleep(0.01)
 
 
 # function that sends velocity commands to turtlebot, calls other functions. Run by main function (see below)
 def run_turtlebot():
+
+    turtle = turtlebot()
+    turtle2 = turtlebot()
+    turtle3 = turtlebot()
+    turtle4 = turtlebot()
+    turtle5 = turtlebot()
+    turtle6 = turtlebot()
+    turtle7 = turtlebot()
+    turtle8 = turtlebot()
     while not rospy.is_shutdown():
-        turtle = turtlebot()
-        if turtle.first_turn:
-            turtle.rotate_turtlebot(0,1)
-            turtle.rotate_turtlebot(1,0)
-            turtle.move_forward(0.01, 0)
+        if turtle.first_turn is True:
+            turtle.move_turtlebot(1)
+            print("Maneuver 1 completed")
+            rospy.sleep(1)
+            turtle2.rotate_turtlebot(90)
+            print("Maneuver 2 completed")
+            rospy.sleep(1)
+            turtle3.move_turtlebot(1)
+            print("Maneuver 3 completed")
+            rospy.sleep(1)
+            turtle4.rotate_turtlebot(90)
+            print("Maneuver 4 completed")
+            rospy.sleep(1)
+            turtle5.move_turtlebot(1)
+            print("Maneuver 5 completed")
+            rospy.sleep(1)
+            turtle6.rotate_turtlebot(90)
+            print("Maneuver 6 completed")
+            rospy.sleep(1)
+            turtle7.move_turtlebot(1)
+            print("Maneuver 7 completed")
+            rospy.sleep(1)
+            turtle8.rotate_turtlebot(90)
+
         turtle.first_turn = False
-        print("Loop")
+        
 
 
 # main function; executes the run_turtlebot function until we hit control + C
